225c57d0320717147846c1a2f7993d590b42ddbc
yangpanRoy <619821473@qq.com>
2016-12-20 22:41:04 +0800

    完成了从json加载数据，且可以从URL加载广告banner图和Item中的imageView图片（非WIFI环境下加载速度有些慢）

--- app/src/main/java/adapter/SearchHistoryItemAdapter.java
+++ app/src/main/java/adapter/SearchHistoryItemAdapter.java
@@ -1,7 +1,12 @@
 package adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,16 +15,23 @@ import android.widget.TextView;
 
 import com.novadata.batteryapp.R;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MyListItemAdapter extends RecyclerView.Adapter {
+public class SearchHistoryItemAdapter extends RecyclerView.Adapter {
     private LayoutInflater inflater;
     private ArrayList<HashMap<String, Object>> listItem;
     private MyItemClickListener myItemClickListener;
+    private String imageUrl;
+    private Bitmap bitmap;
+    private ImageView imageView;
 
-
-    public MyListItemAdapter(Context context, ArrayList<HashMap<String, Object>> listItem) {
+    public SearchHistoryItemAdapter(Context context, ArrayList<HashMap<String, Object>> listItem) {
         inflater = LayoutInflater.from(context);
         this.listItem = listItem;
     }//构造函数，传入数据
@@ -71,7 +83,6 @@ public class MyListItemAdapter extends RecyclerView.Adapter {
 
     }
 
-    //加载Item View的时候根据不同TYPE加载不同的布局
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_cell, parent, false);
@@ -85,9 +96,63 @@ public class MyListItemAdapter extends RecyclerView.Adapter {
             ((Holder) holder).Text1.setText((String) listItem.get(position).get("ItemText1"));
             ((Holder) holder).Text2.setText((String) listItem.get(position).get("ItemText2"));
             ((Holder) holder).Text3.setText((String) listItem.get(position).get("ItemText3"));
-            ((Holder) holder).ima.setImageResource((Integer) listItem.get(position).get("ItemImage"));
+            imageUrl = (String) listItem.get(position).get("ItemImage");
+            imageView = ((Holder) holder).ima;
+            Thread imageViewHandler = new Thread(new NetImageHandler());
+            imageViewHandler.start();
         }
     }//在这里绑定数据到ViewHolder里面
+
+    class NetImageHandler implements Runnable {
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(imageUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setDoInput(true);
+                conn.connect();
+                InputStream is = conn.getInputStream();
+                bitmap = BitmapFactory.decodeStream(is);
+                //发送消息，通知UI组件显示图片
+                handler.sendEmptyMessage(0);
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what == 0){
+                imageView.setImageBitmap(bitmap);
+            }
+        }
+    };
+
+    public static Bitmap getHttpBitmap(String url) {
+        URL myFileUrl = null;
+        Bitmap bitmap = null;
+        try {
+            myFileUrl = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            assert myFileUrl != null;
+            HttpURLConnection conn = (HttpURLConnection) myFileUrl.openConnection();
+            conn.setConnectTimeout(0);
+            conn.setDoInput(true);
+            //conn.connect();
+            InputStream is = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return bitmap;
+    }
 
     @Override
     public int getItemCount() {
